60d7d8c188344c44969e0cb160fcfd99b60dea8c
Ilia Novikov <ilia.novikov@live.ru>
2016-05-19 11:05:52 +0300

    Исправлен класс SafeDict

--- safe_dict.py
+++ safe_dict.py
@@ -1,3 +1,8 @@
 class SafeDict(dict):
+    def __init__(self, d, **kwargs):
+        super().__init__(**kwargs)
+        for key in d.keys():
+            self[key] = d[key]
+
     def __missing__(self, key):
         return '{' + key + '}'
